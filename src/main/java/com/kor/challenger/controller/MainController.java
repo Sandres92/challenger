00d5f1bfaaebeec418d2f29a873a7efb6023d979
Aleksandr Korshunov <eastoutcity@mail.ru>
2020-05-21 21:58:43 +0300

    Initial commit

---
+++ src/main/java/com/kor/challenger/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.kor.challenger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MainController {
+    private int counter = 4;
+
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
+        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
+        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+}
