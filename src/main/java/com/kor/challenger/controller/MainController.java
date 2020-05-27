c46efaff2a58cd745eff134d7727eaf9a17f19fc
Aleksandr Korshunov <eastoutcity@mail.ru>
2020-05-27 07:45:47 +0300

    add connection to client

--- src/main/java/com/kor/challenger/controller/MainController.java
+++ src/main/java/com/kor/challenger/controller/MainController.java
@@ -1,8 +1,7 @@
 package com.kor.challenger.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kor.challenger.exeptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,13 +14,47 @@ public class MainController {
     private int counter = 4;
 
     private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("text", "First message");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("text", "Second message");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("text", "Third message");
+        }});
     }};
 
     @GetMapping
     public List<Map<String, String>> list() {
         return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> listItem(@PathVariable("id") String id) {
+        System.out.println("id   =   " + id);
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+
+        System.out.println("yes");
+
+        message.put("id", String.valueOf(counter++));
+
+        messages.add(message);
+
+        return message;
     }
 }
